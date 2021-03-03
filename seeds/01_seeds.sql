7c2967ba491b7ba0d54266d9f73e07e98a65e0d3
Daniel Tweedie <djtwd.ix@icloud.com>
2021-03-03 14:14:34 +0000

    Write user seeds statement

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, password)
+VALUES ('Daniel James', 'daniel@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), 
+('Scott Mallory', 'scott@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Alex Kennedy', 'alex@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
