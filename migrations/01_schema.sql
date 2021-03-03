c093f16dcb06d2cc653f0eb8134eb1d1329318c8
Daniel Tweedie <djtwd.ix@icloud.com>
2021-03-03 13:12:41 +0000

    Write create properties table statement

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -4,3 +4,21 @@ CREATE TABLE users (
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  cost_per_night INTEGER NOT NULL,
+  parking_spaces SMALLINT,
+  num_bedrooms SMALLINT,
+  num_bathrooms SMALLINT,
+  main_photo TEXT,
+  cover_photo TEXT,
+  active BOOLEAN DEFAULT TRUE,
+  country VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  postal_code VARCHAR(255) NOT NULL
+);
