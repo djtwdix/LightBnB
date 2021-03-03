3a34a409e1ece60320ed8f7af274f7f8e7d6f1cd
Daniel Tweedie <djtwd.ix@icloud.com>
2021-03-03 14:15:42 +0000

    Write reservation seeds statement

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -2,3 +2,8 @@ INSERT INTO users (name, email, password)
 VALUES ('Daniel James', 'daniel@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'), 
 ('Scott Mallory', 'scott@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
 ('Alex Kennedy', 'alex@email.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) 
+VALUES (1, 1, '2021-09-11', '2021-09-26'),
+(2, 2, '2021-03-04', '2019-04-01'),
+(3, 3, '2021-10-01', '2021-10-14');
