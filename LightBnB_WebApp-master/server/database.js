6e29e91dbde7bdff0e5ba93482bf902cef71beee
Daniel Tweedie <djtwd.ix@icloud.com>
2021-03-04 14:40:04 +0000

    Allow options for properties query on search

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -89,14 +89,49 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
+const getAllProperties = function(options, limit) {
+  console.log("limit: ", limit)
+  console.log(options);
+  const queryParams = [];
+  let queryString = `
   SELECT properties.*, ROUND(avg(rating)) as average_rating FROM properties
-  JOIN property_reviews ON properties.id = property_id
-  GROUP BY properties.id
-  LIMIT $1
-  `, [limit])
+  JOIN property_reviews ON properties.id = property_id`;
+  if (options.city) {
+    queryParams.push(`%${options.city}%`)
+    queryString += ` WHERE city LIKE $${queryParams.length}`
+  }
+  if (options.minimum_price_per_night) {
+    queryParams.push(Number(options.minimum_price_per_night) * 100);
+    if (queryParams.length) {
+      queryString += ` AND cost_per_night >= $${queryParams.length}`
+    } else {
+      queryString += ` WHERE cost_per_night >= $${queryParams.length}`
+    }
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(Number(options.maximum_price_per_night) * 100)
+    if (queryParams.length) {
+      queryString += ` AND cost_per_night <= $${queryParams.length}`
+    } else {
+      queryString += ` WHERE cost_per_night <= $${queryParams.length}`
+    }
+  }
+  queryString += `
+  GROUP BY properties.id `
+  
+  if (options.minimum_rating) {
+    queryParams.push(Number(options.minimum_rating))
+    queryString += `HAVING ROUND(avg(rating)) >= $${queryParams.length} `
+  }
+
+  queryParams.push(limit)
+  queryString += `ORDER BY cost_per_night
+  LIMIT $${queryParams.length}
+  `
+  console.log("params: ", queryParams, "string: ", queryString)
+  return pool.query(queryString, queryParams)
   .then(res => {
+    /* console.log(res.rows) */
     return res.rows;
   });
 }
